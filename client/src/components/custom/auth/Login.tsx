720d7225e067cd19865ef36e5f448c62e720848b
utkarsh <utkarshsingh132003@gmail.com>
2025-11-04 21:05:55 +0530

    login with x

--- client/src/components/custom/auth/Login.tsx
+++ client/src/components/custom/auth/Login.tsx
@@ -1,7 +1,8 @@
 const Login = () => {
   return (
-    <div>
-      This is a Login Page
+    <div className="flex justify-center items-center h-screen overflow-y-hidden">
+      <p className="bg-linear-to-br w-[15%] from-gray-900 to-gray-800 text-white flex gap-x-2 items-center justify-center h-[55px] text-xl font-medium rounded-md hover:rounded-br-4xl transition-all ease-in-out duration-300 hover:cursor-pointer hover:scale-110 hover:h-[60px]"><img src="/x.png" alt="X" className="w-8 h-8 text-white" />
+      Login With X</p>
     </div>
   )
 }
